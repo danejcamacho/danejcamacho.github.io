f09c0f62cde5949d8b626bc128efdbec1de76f9b
Dane Camacho <danejcamacho@gmail.com>
2023-03-18 15:09:35 -0700

    monkey-dating host

---
+++ Monkey-Dating/README.md
@@ -0,0 +1,48 @@
+# Monkey Dating
+A primitive dating app for Monkeys! This project is a submission for the CSUF ACM mini-hackathon.
+![2023-03-15 15 32 35](https://user-images.githubusercontent.com/71571206/225458332-ce58bb6a-ab4e-45c2-a775-2e88205651f8.gif)
+
+<img width="601" alt="Screenshot 2023-03-15 at 3 33 56 PM" src="https://user-images.githubusercontent.com/71571206/225458600-0abb9c66-f477-4398-9346-1605d6f3d9af.png">
+
+<img width="518" alt="Screenshot 2023-03-15 at 3 34 07 PM" src="https://user-images.githubusercontent.com/71571206/225458605-899ee149-f449-4d1d-977f-d04fcf251893.png">
+
+<img width="698" alt="Screenshot 2023-03-15 at 3 33 46 PM" src="https://user-images.githubusercontent.com/71571206/225458613-006d7c51-e6cd-4896-99ce-844c27f0b00b.png">
+
+# create-svelte
+
+Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte).
+
+## Creating a project
+
+If you're seeing this, you've probably already done this step. Congrats!
+
+```bash
+# create a new project in the current directory
+npm create svelte@latest
+
+# create a new project in my-app
+npm create svelte@latest my-app
+```
+
+## Developing
+
+Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
+
+```bash
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
+
+## Building
+
+To create a production version of your app:
+
+```bash
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
+
+> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
---
+++ Monkey-Dating/src/routes/about/+page.svelte
@@ -0,0 +1,160 @@
+
+<main>
+    <header>
+        <img class = "logo" src = "/logo.jpg" alt = logo/>
+      <h1>Monkey Dating</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About Us</a></li>
+          <li><a href="/contact">Contact Us</a></li>
+        </ul>
+      </nav>
+    </header>
+<body>
+    <h1>About Us</h1>
+    <br>
+    <h2>Our Values</h2>
+    <p>Here at Monkey Dating, we believe in love at first sight. But nowadays, finding a partner is rough.
+        Monkey Dating makes it easy. <br><br> At Monkey Dating, we're dedicated to helping monkeys find their perfect match. Our goal is to create a safe and fun environment where
+        monkeys of all backgrounds can connect with each other and form meaningful relationships. </p>
+
+    <h2>Frequently Asked Questions</h2>
+    <ul>
+        <li>Q: Is this website only for monkeys?<br>
+            A: Yes, this website is exclusively for monkeys to find compatible partners and make meaningful connections.
+        </li>
+        <li>Q: Can humans join the website?<br>
+        A: No, humans cannot join the website. Our platform is specifically designed for monkeys to interact and communicate with each other.
+        </li>
+        <li>
+        Q: How do I create a profile on the website?<br>
+        A: Our website is highly specialized curated so that with a small fee &#40 $300 a month not including additional fees &#41 we will personally put your profile on the front page of Monkey Dating.
+        </li>
+        <li>
+            Q: How do I know the monkeys I see online are real monkeys?<br>
+            A: First of all, why WOULDN'T they be real monkeys, second of all, they are real dude, stop asking questions. Here's a banana, now go away
+            <img src= "/banana.jpg" alt = banana/>
+        </li>
+        
+    
+    </ul>
+    
+</body>
+<footer>
+    <p>&copy; 2023 Monkey Dating Inc. All rights reserved.</p>
+</footer>
+</main>
+
+
+<style>
+
+    
+nav ul {
+    list-style: none;
+    }
+    
+    * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  
+
+  
+  /* Main styles */
+  main {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+    font-family:   Arial, sans-serif;
+  }
+  
+  /* Header styles */
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+  
+  h1 {
+    font-size: 36px;
+    font-weight: bold;
+    color: #333;
+  }
+  
+  nav ul {
+    list-style: none;
+    display: flex;
+  }
+  
+  nav li {
+    margin-left: 20px;
+  }
+  
+  nav a {
+    text-decoration: none;
+    color: #333;
+    font-weight: bold;
+  }
+  
+  /* Featured section styles */
+  
+  
+  
+  
+  ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  
+  li {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 10px;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.1);
+  }
+  
+  img {
+    width: 300px;
+    height: 300px;
+    
+    object-fit: cover;
+    margin-bottom: 20px;
+  }
+  
+  
+  
+  p {
+    font-size: 16px;
+    color: #777;
+    margin-bottom: 10px;
+  }
+  
+  
+  
+ 
+        
+  .featured ul li img {
+    display: block;
+    margin: 0 auto;
+  }
+  
+  
+  
+  .logo{
+    width: 100px;
+    height: 100px;
+    border-radius: 35%;
+  }
+</style>
